430e12aceb5b1001b1b2e654d515e7f30e0fa300
jwtlt <sinanbozkurt37@gmail.com>
2022-10-06 17:52:04 +0300

    RemoveDuplicates added

--- src/arrayList/RemoveDuplicates.java
+++ src/arrayList/RemoveDuplicates.java
@@ -8,15 +8,16 @@ public class RemoveDuplicates {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(10);
         list.add(10);
-        list.add(15);
-        list.add(15);
+        list.add(10);
         list.add(20);
         list.add(20);
-        list.add(25);
-        list.add(25);
+        list.add(20);
         list.add(30);
         list.add(30);
         list.add(30);
+        list.add(40);
+        list.add(40);
+        list.add(40);
 
         ArrayList<Integer> result = new ArrayList<>(); //{10, 20}
 
